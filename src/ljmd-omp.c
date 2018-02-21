f25d5b01c5ab95e1b39526fdc72837da0c11b39d
martinzh <sstereo.vision@gmail.com>
2018-02-21 14:15:37 -0600

    added omp script

--- src/ljmd-omp.c
+++ src/ljmd-omp.c
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <omp.h>
+
 /* generic file- or pathname buffer length */
 #define BLEN 200
 
@@ -135,6 +137,7 @@ static void velverlet(mdsys_t *sys)
     int i;
 
     /* first part: propagate velocities by half and positions by full step */
+    # pragma omp parallel for
     for (i=0; i<sys->natoms; ++i) {
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
@@ -148,6 +151,7 @@ static void velverlet(mdsys_t *sys)
     force(sys);
 
     /* second part: propagate velocities by another half step */
+    # pragma omp parallel for
     for (i=0; i<sys->natoms; ++i) {
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
